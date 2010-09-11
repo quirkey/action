30f6b35b9c01c1dbb7a4b9e6b5fecbf68389fd78
Aaron Quint <aaron@quirkey.com>
2010-09-10 18:26:26 -0700

    images in config

--- config.js
+++ config.js
@@ -3,6 +3,9 @@
   "mapDirectories": {
     "index.html": "_attachments/index.html",
     "css": "_attachments/css",
+    "images": "_attachments/images",
+    "sass": false,
+    ".sass-cache": false,
     "js": "_attachments/js",
     "templates": "_attachments/templates",
     "db": "",
