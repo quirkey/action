b8194cb45fb5604637ddf918fa28d1f5a4f91b86
Aaron Quint <aaron@quirkey.com>
2010-09-10 21:33:19 -0700

    Added basics of Sammy.Couch

--- js/app.js
+++ js/app.js
@@ -1,27 +1,49 @@
 (function($) {
-  
+
   $.sammy('#container', function() {
     this.use('JSON')
         .use('Meld')
-        .use('Storage');
-    
+        .use('Storage')
+        .use('NestedParams')
+        .use('Couch');
+
     var showLoading = function() {
       $('#loading').show();
     };
-    
+
     var hideLoading = function() {
       $('#loading').hide();
     };
-    
+
+    var Action = this.createModel('action');
+    Action.extend({
+      parse: function(doc) {
+
+      },
+      beforeSave: function(doc) {
+
+      }
+    });
+
     this.bind('run', function() {
       showLoading();
     });
-    
+
     this.get('#/', function(context) {
-      this.load('#action-index')
-          .swap();
+      this.load($('#action-index'))
+          .replace('#main')
+          .then(hideLoading);
+
+      this.db().allDocs(function(docs) {
+        this.log("docs", docs);
+      });
     });
-    
-  });
-  
+
+    this.post('#/action', function(context) {
+      Action.create(this.params['action']);
+    });
+
+  }).run('#/');
+
+
 })(jQuery);
---
+++ js/sammy.couch.js
@@ -0,0 +1,65 @@
+(function($, Sammy) {
+
+  Sammy = Sammy || {};
+
+  Sammy.Couch = function(app, dbname) {
+
+    // set the default dbname form the URL
+    dbname = dbname || window.location.pathname.split('/')[1];
+
+    var db = function() {
+      if (!dbname) {
+        throw("Please define a db to load from");
+      }
+      return this._db = this._db || $.couch.db(dbname);
+    };
+
+    var timestamp = function() {
+      return Math.round(new Date().getTime() / 1000);
+    };
+
+    this.db = db();
+
+    this.createModel = function(type, options) {
+      options = $.extend({
+        defaultDocument: function() {
+          return {
+            type: type,
+            updated_at: timestamp()
+          }
+        },
+        errorHandler: function(response) {
+          app.trigger('error.' + type, {error: error});
+        }
+      }, options || {});
+
+      var mergeCallbacks = function(callback) {
+        var base = {error: options.errorHandler};
+        if ($.isFunction(callback)) {
+          return $.extend(base, {success: callback});
+        } else {
+          return $.extend(base, callback || {})
+        }
+      };
+
+      var mergeDefaultDocument = function(doc) {
+        return $.extend({}, options.defaultDocument(), doc);
+      };
+
+      return {
+        timestamp: timestamp,
+        extend: function(obj) {
+          $.extend(this, obj);
+        },
+        create: function(doc, callback) {
+          return db.saveDoc(mergeDefaultDocument(doc), mergeCallbacks(callback));
+        }
+      }
+    };
+
+    this.helpers({
+      db: db()
+    })
+  };
+
+})(jQuery, window.Sammy);
