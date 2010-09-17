d8087046459a8df9da4897717f91e9526fa0aaac
Aaron Quint <aaron@quirkey.com>
2010-09-17 12:18:16 -0700

    Better compass plugin

--- hooks/before_build.rb
+++ hooks/before_build.rb
@@ -1,3 +1,8 @@
+require 'sass/plugin'
+require 'compass'
+require 'compass/logger'
+require 'jim'
+
 # jim
 jimfile = File.join(app_dir, 'Jimfile')
 logger.debug "bundling js"
@@ -6,4 +11,9 @@ bundler = Jim::Bundler.new(File.read(jimfile), Jim::Index.new(app_dir))
 bundler.bundle!
 
 # compass
-Soca.logger.info "compass: " + `compass compile ../`
+
+Soca.logger.info "compiling compass"
+compass_from = File.join(app_dir, 'sass')
+compass_to   = File.join(app_dir, 'css')
+compass = Compass::Compiler.new(app_dir, compass_from, compass_to, Compass.sass_engine_options)
+compass.run
